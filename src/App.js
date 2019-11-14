ccf189010bd7fdc4fa3a942cf1fd4559969bb6c0
miehedgaard <57318193+miehedgaard@users.noreply.github.com>
2019-11-14 13:21:56 +0100

    Shows Title and Subtitle

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import Image from './Image/Image.js'
 import Knap from './Knap.js'
+import './Knap.css';
+import MyHeader from './MyHeader.js';
 
 function App() {
   return (
     <div className="App">
     <Knap/>
     <Image/>
+    <MyHeader/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class MyHeader extends React.Component {
+    render() {
+        return (
+            <div id="opgave"> 
+            <h1 style = {{color: "black"}}> Opgave 1</h1>
+            <p>Her skal der stå en opgavebeskrivelse</p>
+            </div>
+        );
+    }
+}
 
+ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<MyHeader/>, document.getElementById('opgave'));
+//ReactDOM.render(<MyHeader />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
