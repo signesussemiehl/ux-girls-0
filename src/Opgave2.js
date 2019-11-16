2ab0244e446a659d145877987692513dfeb282f1
miehedgaard <57318193+miehedgaard@users.noreply.github.com>
2019-11-16 17:26:52 +0100

    Created Opgave2 page

---
+++ src/Opgave2.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+class MyForm extends React.Component {
+    render() {
+      return (
+        <form id= "Form">
+          <h1>Opgave 2</h1>
+          <p>Vælg 3 ord der bedst beskriver billedet</p>
+          <input
+            type="text"
+          />
+          <br/>
+          <br/>
+          <input
+            type="text"
+          />
+          <br/>
+          <br/>
+          <input
+            type="text"
+          />
+        </form>
+      );
+    }
+  }
+
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<MyForm />, document.getElementById('Form'));
+
+//ReactDOM.render(<MyHeader />, document.getElementById('root'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
